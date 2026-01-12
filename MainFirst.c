e17e9bcd346c50bf3b84d52fab2529d69f66ad1f
davidfad <davidboy4481@gmail.com>
2026-01-12 18:56:24 +0200

    hey second test

--- MainFirst.c
+++ MainFirst.c
@@ -3,8 +3,17 @@
 #include <string.h>
 #include "Jerry.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    printf("Hello World");
+    printf("Hello World \n");
+
+    if(argc > 1)
+    {
+        printf("%s \n", argv[1]);
+        printf("%s \n", argv[2]);
+        printf("%s \n", argv[3]);
+    }
     return 0;
+
+    
 }
